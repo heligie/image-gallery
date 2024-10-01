85a6086aecda82af99ba7b823e1cf030c2d1afcb
heligie <heligiette@gmail.com>
2024-10-01 23:16:49 +0300

    feat: add a search button and update stats display

--- js/gallery/stats.js
+++ js/gallery/stats.js
@@ -1,15 +1,19 @@
-const downloads = document.querySelector('.gallery__stats-count--downloads');
-const views = document.querySelector('.gallery__stats-count--views');
-const developers = document.querySelector('.gallery__stats-count--developers');
-const photos = document.querySelector('.gallery__stats-count--photos');
-const photographers = document.querySelector('.gallery__stats-count--photographers');
+import { divideIntoDigitPlace } from '../utils/utils.js';
+
+const downloadsCount = document.querySelector('.gallery__stats-count--downloads');
+const viewsCount = document.querySelector('.gallery__stats-count--views');
+const developersCount = document.querySelector('.gallery__stats-count--developers');
+const photosCount = document.querySelector('.gallery__stats-count--photos');
+const photographersCount = document.querySelector('.gallery__stats-count--photographers');
 
 const renderStats = (stats) => {
-  downloads.textContent = stats.downloads;
-  views.textContent = stats.views;
-  developers.textContent = stats.new_developers;
-  photos.textContent = stats.new_photos;
-  photographers.textContent = stats.new_photographers;
+  const { downloads, views, new_developers, new_photos, new_photographers } = stats;
+
+  downloadsCount.textContent = divideIntoDigitPlace(downloads);
+  viewsCount.textContent = divideIntoDigitPlace(views);
+  developersCount.textContent = divideIntoDigitPlace(new_developers);
+  photosCount.textContent = divideIntoDigitPlace(new_photos);
+  photographersCount.textContent = divideIntoDigitPlace(new_photographers);
 };
 
 export { renderStats };
